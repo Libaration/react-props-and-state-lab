1ffe079f8804b8398ea4368ba821193d215c9c37
Cristian Deleon <libaration@gmail.com>
2021-06-11 17:04:50 -0400

    commit tree d9ae2801a1401612538e55d2c608f421a550f16a

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from "react";
 
-import Filters from './Filters'
-import PetBrowser from './PetBrowser'
+import Filters from "./Filters";
+import PetBrowser from "./PetBrowser";
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       pets: [],
       filters: {
-        type: 'all'
-      }
-    }
+        type: "all",
+      },
+    };
   }
 
   render() {
@@ -24,16 +24,46 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+                onChangeType={this.handleChangeType}
+                onFindPetsClick={this.handlePetsClick}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
+
+  handleChangeType = (type) => {
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: type,
+      },
+    });
+  };
+
+  handlePetsClick = async () => {
+    let url = `/api/pets?type=${this.state.filters.type}`;
+    if (this.state.filters.type === "all") {
+      url = "/api/pets";
+    }
+    const response = await fetch(url);
+    const pets = await response.json();
+    this.setState({
+      pets: pets,
+    });
+  };
+  onAdoptPet = (petId) => {
+    const pets = this.state.pets.map((p) => {
+      return p.id === petId ? { ...p, isAdopted: true } : p;
+    });
+    this.setState({ pets: pets });
+  };
 }
 
-export default App
+export default App;
